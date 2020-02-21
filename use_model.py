5a8881a379574da9d1ed05c9d7b06497f0324f14
Utkarsh Mishra <ug19981@gmail.com>
2020-02-21 07:39:39 +0530

    Update use_model.py

--- use_model.py
+++ use_model.py
@@ -3,12 +3,12 @@ import cv2
 import numpy as np
 
 
-json_file = open('model.json', 'r')
+json_file = open('model1.json', 'r')
 loaded_model_json = json_file.read()
 json_file.close()
 loaded_model = model_from_json(loaded_model_json)
 
-loaded_model.load_weights("model.h5")
+loaded_model.load_weights("model1.h5")
 print("Loaded model from disk")
 
 '''loaded_model.compile(loss=keras.losses.categorical_crossentropy,
@@ -17,10 +17,10 @@ print("Loaded model from disk")
 '''
 loaded_model.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
 
-img = cv2.imread("./dogs_cates/test/21.jpg")
-img = cv2.resize(img, (50,50))
+img = cv2.imread("./test4.png")
+img = cv2.resize(img, (200,200))
 print(img.shape)
-img = img.reshape(1, 50, 50, 3)
+img = img.reshape(1, 200, 200, 3)
 
 print(img.shape)
 #print(np.argmax(loaded_model.predict(img)))
